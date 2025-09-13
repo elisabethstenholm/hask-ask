d81ca24be4f51bb486cb3e4e404b11cced1606ac
Elisabeth Stenholm <elisabeth@stenholm.one>
2025-09-13 21:53:37 +0200

    Add timer

--- app/Main.hs
+++ app/Main.hs
@@ -3,41 +3,84 @@ module Main where
 import Control.Concurrent
 import Control.Concurrent.STM
 import Control.Monad
+import Data.Time.Clock
 import Utilities
+import Data.Decimal
 
 type Name = String
 
-type Amount = Float
+type Amount = Decimal
 
 type Bid = (Name, Amount)
 
-type HighestBid = TVar Bid
+data ItemState = Open | Closed
+
+data Item = Item
+  { endTime :: UTCTime,
+    highestBid :: TVar Bid,
+    state :: TVar ItemState
+  }
 
 type BidQueue = TChan Bid
 
-updateCurrentBid :: HighestBid -> BidQueue -> IO ()
-updateCurrentBid currentBidVar queueVar = atomically $ do
-  (name, amount) <- readTVar currentBidVar
-  (bidName, bidAmount) <- readTChan queueVar
-  let newHighestBid = if amount >= bidAmount then (name, amount) else (bidName, bidAmount)
-  writeTVar currentBidVar newHighestBid
+updateCurrentBid :: Item -> BidQueue -> STM ()
+updateCurrentBid item queueVar = do
+  itemState <- readTVar $ state item
+  case itemState of
+    Closed -> return ()
+    Open -> do
+      (name, amount) <- readTVar $ highestBid item
+      (bidName, bidAmount) <- readTChan queueVar
+      let newHighestBid = if amount >= bidAmount then (name, amount) else (bidName, bidAmount)
+      writeTVar (highestBid item) newHighestBid
 
-getBids :: HighestBid -> BidQueue -> IO ()
-getBids currentBidVar queueVar = do
-  (name, amount) <- readTVarIO currentBidVar
-  putStrLn $ "Current bid: " ++ show amount ++ " by " ++ name
-  bidName <- getLine
-  bidAmount <- getLine
-  writeTChanIO queueVar (bidName, read bidAmount)
-  threadDelay 10000
-  getBids currentBidVar queueVar
+updateItem :: Item -> BidQueue -> IO Bid
+updateItem item queueVar = do
+  itemState <- readTVarIO $ state item
+  case itemState of
+    Closed -> readTVarIO $ highestBid item
+    Open -> do
+      atomically $ updateCurrentBid item queueVar
+      updateItem item queueVar
+
+getBids :: Item -> BidQueue -> IO ()
+getBids item queueVar = do
+  itemState <- readTVarIO $ state item
+  case itemState of
+    Closed -> return ()
+    Open -> do
+      (name, amount) <- readTVarIO (highestBid item)
+      putStrLn $ "Current bid: " ++ show amount ++ " by " ++ name
+      bidName <- getLine
+      bidAmount <- getLine
+      writeTChanIO queueVar (bidName, read bidAmount)
+      threadDelay 10000
+      getBids item queueVar
+
+timer :: Item -> IO ()
+timer item = do
+  currentTime <- getCurrentTime
+  if currentTime >= endTime item
+    then
+      writeTVarIO (state item) Closed
+    else do
+      threadDelay 100000
+      timer item
 
 main :: IO ()
 main = do
-  putStrLn "Today's auction item is a coffee cup made entirely of cat purrs!"
+  putStrLn "Today's auction item is a coffee cup made entirely of cat purrs!\n\nYou have 10 seconds!"
 
+  currentTime <- getCurrentTime
+  let itemEndTime = addUTCTime (secondsToNominalDiffTime 10) currentTime
   currentBidVar <- newTVarIO ("", 0)
+  currentState <- newTVarIO Open
   queueVar <- newTChanIO
 
-  forkIO $ forever $ updateCurrentBid currentBidVar queueVar
-  getBids currentBidVar queueVar
+  let item = Item {endTime = itemEndTime, highestBid = currentBidVar, state = currentState}
+
+  void $ forkIO $ forever $ timer item
+  void $ forkIO $ do
+    (winner, _) <- updateItem item queueVar
+    putStrLn $ "Winning bid: " ++ winner
+  getBids item queueVar
--- app/Utilities.hs
+++ app/Utilities.hs
@@ -2,5 +2,8 @@ module Utilities where
 
 import Control.Concurrent.STM
 
+writeTVarIO :: TVar a -> a -> IO ()
+writeTVarIO = (atomically .) . writeTVar
+
 writeTChanIO :: TChan a -> a -> IO ()
 writeTChanIO = (atomically .) . writeTChan
